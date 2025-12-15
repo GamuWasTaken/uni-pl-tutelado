d320a025a2cddc558cbdcf91f68edb00407898b0
GamuIsAlreadyTaken <gamudiez@gmail.com>
2025-12-15 20:42:11 +0100

    generic fat array

--- repli.c
+++ repli.c
@@ -2,64 +2,66 @@
 #include <stdlib.h>
 #include <string.h>
 
-// TODO make a template for generics, so we can reuse this :)
-typedef struct {
-	int len;
-	int* data;
-} Array;
+#define _IMPL_ARRAY(_type, _name) \
+typedef struct {                  \
+	int len;                        \
+	_type* data;                    \
+} _name;                          \
+\
+_name _name##_new(int len, _type* data) {     \
+	_name arr;                                  \
+	arr.len = len;                              \
+	arr.data = malloc(sizeof(_type) * arr.len); \
+	                                            \
+	if(arr.data == NULL) {                      \
+		arr.len = 0;                              \
+		return arr;                               \
+	}                                           \
+	                                            \
+	memcpy(arr.data, data, sizeof(_type)*len);  \
+	return arr;                                 \
+}                                             \
+\
+_name _name##_of_single(_type num) { \
+	return _name##_new(1, &num);       \
+}                                    \
+\
+_name _name##_concat(_name a, _name b) {                    \
+	_name arr;                                                \
+	arr = _name##_new(a.len + b.len, a.data);                 \
+	                                                          \
+	int offset = a.len;                                       \
+	if(arr.data == NULL) offset = 0;                          \
+	                                                          \
+	memcpy(arr.data + offset, b.data, sizeof(_type) * b.len); \
+	                                                          \
+	return arr;                                               \
+}                                                           \
+                                                            \
+void _name##_free(_name arr) {                              \
+	if(arr.data == NULL) return;                              \
+	free(arr.data);                                           \
+}                                                           \
 
-Array array_new(int len, int* data) {
-	Array arr;
-	arr.len = len;
-	arr.data = malloc(sizeof(int) * arr.len);
-
-	if(arr.data == NULL) {
-		arr.len = 0;
-		return arr;
-	}
-
-	memcpy(arr.data, data, sizeof(int)*len);
-	return arr;
-}
-
-Array array_of_int(int num) {
-	return array_new(1, &num);
-}
-
-Array array_concat(Array a, Array b) {
-	Array arr;
-	arr = array_new(a.len + b.len, a.data);
-
-	int offset = a.len;
-	if(arr.data == NULL) offset = 0;
-	
-	memcpy(arr.data + offset, b.data, sizeof(int) * b.len);
-	
-	return arr;
-}
-
-void array_free(Array arr) {
-	if(arr.data == NULL) return;
-	free(arr.data);
-}
+_IMPL_ARRAY(int, Ints)
 
 int main(void) {
 	int from[3] = {1,2,3};
 	int empty[0] = {};
-	Array arr1 = array_new(3, from);
-	Array arr2 = array_new(0, empty);
+	Ints arr1 = Ints_new(3, from);
+	Ints arr2 = Ints_new(0, empty);
 
-	Array arr3 = array_of_int(atoi("8"));
+	Ints arr3 = Ints_of_single(atoi("8"));
 
-	Array arr = array_concat(arr3, arr3);
+	Ints arr = Ints_concat(arr3, arr3);
 
 	for (int i = 0; i < arr.len; i++) {
 		printf("%d, %d\n", i, arr.data[i]);
 	}
 
-	array_free(arr1);
-	array_free(arr2);
-	array_free(arr);
+	Ints_free(arr1);
+	Ints_free(arr2);
+	Ints_free(arr);
 
 	return 0;
 }
