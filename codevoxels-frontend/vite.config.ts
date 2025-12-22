a4228b6e241b0efbf8a50cf795e160c1d721822d
manuelsrleon <manuelsrleon@gmail.com>
2025-12-22 22:33:52 +0100

    edit base-url

--- codevoxels-frontend/vite.config.ts
+++ codevoxels-frontend/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: '/uni-pl-tutelado/',
   plugins: [reactRouter(), tsconfigPaths()],
   server: {
     watch: {
