f215056a0fc46f2a21ac5b94e0289f59237f7a1d
manuelsrleon <manuelsrleon@gmail.com>
2025-12-22 21:21:55 +0100

    add missing Links tag in root

--- codevoxels-frontend/app/root.tsx
+++ codevoxels-frontend/app/root.tsx
@@ -1,26 +1,12 @@
 import {
 
+  Links,
   Meta,
   Outlet,
   Scripts,
 } from "react-router";
 
-import type { Route } from "./+types/root";
-
-import "./app.css";
-
-export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Bakbak+One&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
+import "./root.css";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -29,6 +15,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="" initial-scale="1" />
         <Meta></Meta>
+        <Links></Links>
       </head>
       <body>
         {children}
