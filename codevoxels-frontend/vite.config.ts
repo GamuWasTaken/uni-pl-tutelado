0763d84c4ce0fb9ff73969d8433755e3934cfaf1
manuelsrleon <manuelsrleon@gmail.com>
2025-12-23 17:16:46 +0100

    switch basename to react router config

--- codevoxels-frontend/vite.config.ts
+++ codevoxels-frontend/vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: '/uni-pl-tutelado/',
   plugins: [reactRouter(), tsconfigPaths()],
   server: {
     watch: {
